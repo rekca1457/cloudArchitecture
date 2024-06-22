aa830b5b167c6daaba92c4443ed9e613aeef4768
rekca1457 <rekca@tutamail.com>
2024-06-22 23:05:59 +0300

    update environment variables

---
+++ src/VerwunschenerWald.java
@@ -0,0 +1,19 @@
+public abstract class VerwunschenerWald {
+
+    protected Position position;
+    protected int schaden = 0;
+
+    public VerwunschenerWald(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public int getSchaden() {
+        return schaden;
+    }
+
+    public abstract String getName();
+}
