0d54bac93e8a71d0f5a5babed5470ab2e0dab378
rekca1457 <rekca@tutamail.com>
2024-06-25 23:05:59 +0300

    Improve security

---
+++ src/Maerchen.java
@@ -0,0 +1,9 @@
+public class Maerchen {
+
+    public static void main (String [] args){
+        Maerchenwelt m = new Maerchenwelt(10,10,10,0);
+            // for(int i = 0; i <= 10; i++) {
+                 m.start();
+      // }
+    }
+}
