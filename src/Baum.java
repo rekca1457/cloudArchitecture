ab9d84e36af94c2b79d1fffe048770865afcff96
rekca1457 <rekca@tutamail.com>
2024-06-26 23:05:59 +0300

    update api documentation

---
+++ src/Baum.java
@@ -0,0 +1,11 @@
+public class Baum extends VerwunschenerWald{
+
+    public Baum(Position position) {
+        super(position);
+    }
+
+    @Override
+    public String getName() {
+        return "B";
+    }
+}
